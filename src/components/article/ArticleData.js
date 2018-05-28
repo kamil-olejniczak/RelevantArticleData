8d3cfd3431f309e6418953931342c638d928ff15
Kreattehak <nerokamil@gmail.com>
2018-05-28 07:44:06 +0200

    Update article data to show parsed response, add css

--- src/components/article/ArticleData.js
+++ src/components/article/ArticleData.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './ArticleData.css';
 
-const ArticleData = () => (
-    <div>ArticleData is working!</div>
-);
+const ArticleData = ({data}) => {
+    const title = data.title && (<p>You are currently reading: "{data.title}".</p>);
+    const datePublished = data.date_published && ( <p>Which was published on {data.date_published}.</p>);
+    const domain = data.domain && (<p>On "{data.domain}" domain.</p>);
+    const dek = data.dek && (<p>Headline of the story (dek): "{data.dek}".</p>);
+    const excerpt = data.excerpt && (<p>Hand-crafted summary of content (excerpt): "{data.excerpt}".</p>);
+    const wordCount = data.word_count && (<p>This article has: {data.word_count} words.</p>);
+    const leadImage = data.lead_image_url && (<div><p>Lead image to help you memorise it:</p>
+        <img className="leadImg" src={data.lead_image_url} alt={data.excerpt}/></div>);
+    return (
+        <div className="ArticleData">
+            <span id="close" onClick={() => console.log('onClick span #close')}><i
+                className='far fa-times-circle'/></span>
+            {title}
+            {datePublished}
+            {domain}
+            {dek}
+            {excerpt}
+            {wordCount}
+            {leadImage}
+        </div>
+    );
+};
 
 export default ArticleData;
