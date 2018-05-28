e3600b062621189e646977fe1016500ece6c354a
Kreattehak <nerokamil@gmail.com>
2018-05-28 07:41:51 +0200

    Update store to use redux sagas

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,18 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import articleReducer from './reducers/article';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas/sagaIndex';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware));
 
 const rootReducer = combineReducers({article: articleReducer});
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, enhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
