08ce1221fa53ccf1a8caac033df57980a9dd76a7
Kreattehak <nerokamil@gmail.com>
2018-05-29 07:28:30 +0200

    Update store instance to be created with initial state

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
-import articleReducer from './reducers/article';
+import articleReducer, {initialState} from './reducers/article';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas/sagaIndex';
+import {parseDateForRecentlyVisitedFromLocalStorage} from '../utils/utils';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -11,7 +12,21 @@ const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware));
 
 const rootReducer = combineReducers({article: articleReducer});
 
-const store = createStore(rootReducer, enhancers);
+const getInitialState = () => {
+    if (localStorage.getItem('tagStats') || localStorage.getItem('recentVisit')) {
+        return {
+            ...initialState,
+            statistics: JSON.parse(localStorage.getItem('tagStats')),
+            recentlyVisited: parseDateForRecentlyVisitedFromLocalStorage(JSON.parse(localStorage.getItem('recentVisit')))
+        };
+    }
+
+    return initialState;
+};
+
+const reducersInitialState = {article: getInitialState()};
+
+const store = createStore(rootReducer, reducersInitialState, enhancers);
 
 sagaMiddleware.run(rootSaga);
 
