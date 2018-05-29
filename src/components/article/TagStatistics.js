26a4ad5ba6ed27d35bfe23bde9b68394cd931ce0
Kreattehak <nerokamil@gmail.com>
2018-05-29 07:40:09 +0200

    Update TagStatistics with logic - transform a map to an array

--- src/components/article/TagStatistics.js
+++ src/components/article/TagStatistics.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import uuid from 'uuid';
+import './TagStatistics.css';
 
-const TagStatistics = () => (
-    <div>TagStatistics is working!</div>
-);
+class TagStatistics extends Component {
+    mapHashTable() {
+        const object = this.props.tagStats;
+        const arrayWithTagData = [];
+        for (let key in object) {
+            arrayWithTagData.push({
+                id: uuid(),
+                name: key,
+                occurenceCount: object[key]
+            });
+        }
+        arrayWithTagData.sort((first, second) => {
+            return -1 * (first.occurenceCount - second.occurenceCount);
+        });
+        return arrayWithTagData.slice(0, 10);
+    }
+
+    render() {
+        return (
+            <div className="TagStatistics">
+                <span className="header">Tag stats of all time:</span>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Tag Name</th>
+                        <th>Occurrences</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.mapHashTable().map((tag) => (
+                        <tr key={tag.id}>
+                            <td>{tag.name}</td>
+                            <td>{tag.occurenceCount}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
 
 export default TagStatistics;
