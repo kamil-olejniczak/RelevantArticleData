c97b6391341757b9510e9dbe40160b655358bc3c
Kreattehak <nerokamil@gmail.com>
2018-05-29 07:38:58 +0200

    Add JSX to RecentlyVisitedArticles component

--- src/components/article/RecentlyVisitedArticles.js
+++ src/components/article/RecentlyVisitedArticles.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './RecentlyVisitedArticles.css';
 
-const RecentlyVisitedArticles = () => (
-    <div>RecentlyVisitedArticles is working!</div>
+const RecentlyVisitedArticles = (props) => (
+    <div className="RecentlyVisitedArticles">
+        <span className="header">Recently Visited Articles:</span>
+        <span id="resetStats" onClick={props.clearData}>Reset stats</span>
+        {props.recentlyVisited.map((article, index) => (
+            <div key={article.id}>
+                <Link to="/recentlyVisited">
+                    Title: {article.title.length > 40 ? 'Title to long, show detailed statistics.' : article.title}
+                </Link>
+                <br/>
+                <a href={article.url}>
+                    Url: {article.url.length > 40 ? 'Link to long, click here to follow it.' : article.url}
+                </a>
+                <hr/>
+            </div>
+        ))}
+    </div>
 );
 
 export default RecentlyVisitedArticles;
